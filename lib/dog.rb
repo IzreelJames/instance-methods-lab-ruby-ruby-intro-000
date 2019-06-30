891bfb2a527339e904534a94c498f147845ad77e
Izreel James <izreeljames@gmail.com>
2019-06-30 21:16:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog 
+  
   def bark
     puts "Woof!"
     end
     
     def sit 
-      puts "The dog is sitting!"
+      puts "The Dog is sitting!"
     end
    end 
    
-   fido = Dog.new 
-   fido.bark
+   
    
